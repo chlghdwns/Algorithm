ffa46da0b822f9bf48bffdc28d204e60069524ab
최홍준 <111585117+dal-2@users.noreply.github.com>
2022-09-21 23:58:25 +0900

    [level 1] Title: 시저 암호, Time: 0.47 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/README.md
+++ 프로그래머스/lv1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 32.01 ms
+메모리: 65 MB, 시간: 0.47 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,16 @@
 class Solution {
     public String solution(String s, int n) {
-		char[] ss = s.toCharArray();
-		
+        char[] ss = s.toCharArray();
+
 		for (int i = 0; i < ss.length; i++) {
 			if (ss[i] == ' ') {
 				continue;
 			}
-		
-		char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
-		ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
+
+            char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
+            ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
 		}
-	
+
 		return new String(ss);
     }
 }
