08d4737648120db76884f5a2b0c9023737f7c298
최홍준 <111585117+dal-2@users.noreply.github.com>
2022-09-21 23:44:08 +0900

    Update 시저 암호.java

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,27 +1,16 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        
-        for(int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if(ch == ' ') {
-				answer += ch;
+        char[] ss = s.toCharArray();
+
+		for (int i = 0; i < ss.length; i++) {
+			if (ss[i] == ' ') {
 				continue;
 			}
-			if(ch >= 'a' && ch <= 'z') {
-				if(ch + n > 'z') {
-					answer += (char)(ch - 26 + n);
-				}else {
-					answer += (char)(ch + n);
-				}
-			}else if(ch >= 'A' && ch <= 'Z') {
-				if(ch + n > 'Z') {
-					answer += (char)(ch - 26 + n);
-				}else {
-					answer += (char)(ch + n);
-				}
-			}
+
+            char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
+            ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
 		}
-		return answer;
+
+		return new String(ss);
     }
 }
