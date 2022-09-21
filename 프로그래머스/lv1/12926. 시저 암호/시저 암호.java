4c1c953178eb3394faa1ab016558690ba91cd3fa
최홍준 <111585117+dal-2@users.noreply.github.com>
2022-09-21 23:48:48 +0900

    Update 시저 암호.java

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -4,13 +4,11 @@ class Solution {
 
 		for (int i = 0; i < ss.length; i++) {
 			if (ss[i] == ' ') {
-				continue;
-			}
-
-            char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
-            ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
+			continue;
 		}
-
-		return new String(ss);
+		char firstAlphabet = ss[i] >= 'a' ? 'a' : 'A';
+		ss[i] = (char)(firstAlphabet + (ss[i] - firstAlphabet + n) % 26);
+		}
+	return new String(ss);
     }
 }
